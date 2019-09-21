47f2ca07f1bcb651e7b6531db84280175322eacb
brenomec <breno.portela@gmail.com>
2019-09-20 22:46:21 -0300

    first commit

---
+++ build/turtlesim_cleaner/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/breno/turtle_ws/src/turtlesim_cleaner
+# Build directory: /home/breno/turtle_ws/build/turtlesim_cleaner
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
